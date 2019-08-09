89a519cb74572a0cc0e442776cc187cdb85d0835
Jack <0805jauy9131253@gmail.com>
2019-08-09 08:47:43 +0800

    rename dist directory

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 const srcPath = path.resolve(__dirname, 'src');
-const distPath = path.resolve(__dirname, 'dist');
+const distPath = path.resolve(__dirname, 'docs');
 
 module.exports = {
     context: srcPath,
